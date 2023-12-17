2c0b776e9b8970162238728aefc222f64a582d4f
william <nixon@williams-MacBook-Air.local>
2023-12-17 12:08:42 +0700

    revise speaking

--- src/screens/SpeakingSkill/SpeakingSkill.jsx
+++ src/screens/SpeakingSkill/SpeakingSkill.jsx
@@ -9,15 +9,27 @@ import { useNavigate } from "react-router-dom";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { useRef, useEffect } from "react";
 import botspek from "../../../public/img/botspek.png";
 import { useSearchParams } from "react-router-dom";
 
 export const SpeakingSkillAudio = () => {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+  const progressBarRef = useRef({ current: { value: 0, max: 5 } });
   const { transcript, resetTranscript } = useSpeechRecognition();
   let [searchParams, setSearchParams] = useSearchParams();
   let init = searchParams.get("init");
+  const [openInit, setOpenInit] = useState(true);
+
+  useEffect(() => {
+    progressBarRef.current.max = 5;
+    progressBarRef.current.value = 0;
+    // progressBarRef.current.style.setProperty(
+    //   "--range-progress",
+    //   `${(progressBarRef.current.value / 5) * 100}%`
+    // );
+  }, []);
 
   return (
     <>
@@ -67,10 +79,62 @@ export const SpeakingSkillAudio = () => {
           </div>
         </div>
       </Modal>
+      <Modal open={openInit} setOpen={setOpenInit}>
+        <div className="modal-content">
+          <div className="modal-illust">
+            <Question />
+          </div>
+          <div className="modal-header">
+            <p className="modal-title">Petunjuk pengerjaan</p>
+          </div>
+          <div className="modal-body">
+            <p
+              className="modal-text"
+              style={{
+                textAlign: "center",
+              }}
+            >
+              Pada bagian ini, anda dapat menekan tombol mic untuk merekam suara anda, layaknya sedang menggunakan fitur voice note. Robot kemudian akan membalas percakapan anda!
+            </p>
+          </div>
+          <div className="modal-footer">
+            <Button
+              hierachy={"primary"}
+              size={"large"}
+              style={{ marginRight: "16px" }}
+              className="modal-button"
+              onClick={() => {
+                setOpenInit(false);
+                // if (init) {
+                //   navigate("/skill-builder/listening?init=true");
+                //   return;
+                // }
+                // navigate("/skill-builder/speaking/result");
+              }}
+              type="default"
+              text="Mantab"
+            />
+            {/* <Button
+              hierachy={"secondary"}
+              size={"large"}
+              className="modal-button"
+              onClick={() => setOpen(false)}
+              type="default"
+              text="Batal"
+            /> */}
+          </div>
+        </div>
+      </Modal>
       <div className="listening-skill">
         <div className="body">
           <Header title="Speaking Skill Builder" showAlert />
           <div className="main group">
+            <div className="progress">
+              <input type="range" ref={progressBarRef} />
+              <div className="text-wrapper-2">
+                {progressBarRef.current?.value ?? 0}/5
+              </div>
+            </div>
             <img
               className="freepik-robot-inject"
               alt="Freepik robot inject"
@@ -89,6 +153,7 @@ export const SpeakingSkillAudio = () => {
             transcript={transcript}
             resetTranscript={resetTranscript}
             SpeechRecognition={SpeechRecognition}
+            progressBarRef={progressBarRef}
           />
           <Button
             className="design-component-instance-node"
